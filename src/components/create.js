aab44f7b6fa1145285ee80debe222f1a6687efd6
Ryan Harte <ryanharte1997@gmail.com>
2023-01-05 17:19:57 +0000

    The Final Final Commit

--- src/components/create.js
+++ src/components/create.js
@@ -13,7 +13,7 @@ export class Create extends React.Component {
         this.state = {
             pName: '',
             pImg: '',
-            pType: ''
+            pType: 'Normal'
         }
     }
 
@@ -82,12 +82,32 @@ export class Create extends React.Component {
 
                     <div className="form-group">
                         <label>Add Pokemon Type: </label>
-                        <input type="text"
-                            className="form-control"
-                            value={this.state.pType}
-                            onChange={this.onChangePokemonType}
-                        />
+                        <br>
+                        </br>
+                        <br>
+                        </br>
+                        <select value={this.state.pType} onChange={this.onChangePokemonType}>
+                            <option value="Normal">Normal</option>
+                            <option value="Fire">Fire</option>
+                            <option value="Water">Water</option>
+                            <option value="Electric">Electric</option>
+                            <option value="Grass">Grass</option>
+                            <option value="Ice">Ice</option>
+                            <option value="Fighting">Fighting</option>
+                            <option value="Poison">Poison</option>
+                            <option value="Ground">Ground</option>
+                            <option value="Flying">Flying</option>
+                            <option value="Psychic">Psychic</option>
+                            <option value="Bug">Bug</option>
+                            <option value="Rock">Rock</option>
+                            <option value="Ghost">Ghost</option>
+                            <option value="Dragon">Dragon</option>
+                            <option value="Dark">Dark</option>
+                            <option value="Steel">Steel</option>
+                        </select>
                     </div>
+                    <br>
+                    </br>
                     <br>
                     </br>
                     <input type="submit" value="Add Pokemon" />
